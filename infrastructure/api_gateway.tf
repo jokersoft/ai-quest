b341e94c6c394c44d8d901527f4da1f623dcc608
Yaroslav Sklabinskyi <11043232+jokersoft@users.noreply.github.com>
2023-08-06 22:22:33 +0200

    feat: CORS, fix: messages query (#4)

--- infrastructure/api_gateway.tf
+++ infrastructure/api_gateway.tf
@@ -11,9 +11,6 @@ resource "aws_api_gateway_deployment" "lambda_api" {
   depends_on = [
     aws_api_gateway_integration.lambda_integration,
     aws_api_gateway_method.proxy_method,
-#    aws_api_gateway_integration.options_integration,
-#    aws_api_gateway_method.proxy_method_options,
-#    aws_api_gateway_method_response.options_response_200
   ]
 
   lifecycle {
@@ -29,10 +26,10 @@ resource "aws_api_gateway_stage" "dev" {
 
 # root proxy method
 resource "aws_api_gateway_method" "root_proxy_method" {
-  rest_api_id        = aws_api_gateway_rest_api.lambda_api.id
-  resource_id        = aws_api_gateway_rest_api.lambda_api.root_resource_id
-  http_method        = "ANY"
-  authorization      = "NONE"
+  rest_api_id   = aws_api_gateway_rest_api.lambda_api.id
+  resource_id   = aws_api_gateway_rest_api.lambda_api.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "root_proxy_integration" {
@@ -75,41 +72,11 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   content_handling = "CONVERT_TO_TEXT"
 }
 
-# OPTIONS
-#resource "aws_api_gateway_method" "proxy_method_options" {
-#  rest_api_id        = aws_api_gateway_rest_api.lambda_api.id
-#  resource_id        = aws_api_gateway_resource.proxy.id
-#  http_method        = "OPTIONS"
-#  authorization      = "NONE"
-#}
-#
-#resource "aws_api_gateway_integration" "options_integration" {
-#  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
-#  resource_id = aws_api_gateway_resource.proxy.id
-#  http_method = aws_api_gateway_method.proxy_method_options.http_method
-#  type        = "MOCK"
-#}
-#
-#resource "aws_api_gateway_method_response" "options_response_200" {
-#  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
-#  resource_id = aws_api_gateway_resource.proxy.id
-#  http_method = aws_api_gateway_method.proxy_method_options.http_method
-#  status_code = "200"
-#  response_models     = {
-#    "application/json" = "Empty"
-#  }
-#  response_parameters = {
-#    "method.response.header.Access-Control-Allow-Headers" = false
-#    "method.response.header.Access-Control-Allow-Methods" = false
-#    "method.response.header.Access-Control-Allow-Origin"  = false
-#  }
-#}
-
 # IAM
 resource "aws_lambda_permission" "apigw" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.api_lambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn = "${aws_api_gateway_rest_api.lambda_api.execution_arn}/*/*"
+  source_arn    = "${aws_api_gateway_rest_api.lambda_api.execution_arn}/*/*"
 }
