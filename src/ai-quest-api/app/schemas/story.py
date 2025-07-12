80286429194f062dc204d472e13be70ca5dc6178
Yaroslav Sklabinskyi <11043232+jokersoft@users.noreply.github.com>
2025-07-12 09:43:43 +0100

    chapters response (#19)
    * chapters response
    
    * move docs
    
    * fix Chapter.id bytes to uuid conversion
    
    * cleanup id mutation
    
    ---------
    
    Co-authored-by: Yaroslav Sklabinskyi <yaroslav@reliant.ai>

--- src/ai-quest-api/app/schemas/story.py
+++ src/ai-quest-api/app/schemas/story.py
@@ -1,14 +1,45 @@
-from pydantic import BaseModel
-from app.schemas import message
+from pydantic import BaseModel, Field, field_validator
+from typing import Union
+import uuid
 
-class Story(BaseModel):
-    id: str
-    user_id: str
-    title: str | None = None
+
+class Chapter(BaseModel):
+    id: Union[str, bytes, uuid.UUID] = Field(alias='id')
+    narration: str
+    situation: str
+    choices: list[str]
+    action: str
+    outcome: str
+    number: int
+
+    @field_validator('id', mode='before')
+    def convert_id(cls, v: Union[bytes, uuid.UUID, str]) -> str:
+        if isinstance(v, bytes):
+            return str(uuid.UUID(bytes=v))
+        if isinstance(v, uuid.UUID):
+            return str(v)
+        return v
 
     class Config:
         from_attributes = True
 
+
+class Story(BaseModel):
+    id: Union[str, bytes, uuid.UUID] = Field(alias='id')
+    user_id: Union[str, bytes, uuid.UUID] = Field(alias='user_id')
+    title: str | None = None
+
+    @field_validator('id', 'user_id', mode='before')
+    def convert_id(cls, v: Union[bytes, uuid.UUID, str]) -> str:
+        if isinstance(v, bytes):
+            return str(uuid.UUID(bytes=v))
+        if isinstance(v, uuid.UUID):
+            return str(v)
+        return v
+
+    class Config:
+        from_attributes = True
+
+
 class FullStory(Story):
-    messages: list[message.Message]
-    choices: list[str]
+    chapters: list[Chapter]
